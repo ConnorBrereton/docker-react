de30ae86b22889b20a63d1a2b33090c181d5e67b
Connor <cobreret@cisco.com>
2019-06-19 11:51:51 -0700

    minor change to simulate dev team w/ feature branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          React is awesome!
+          React is cool!
         </p>
       </header>
     </div>
